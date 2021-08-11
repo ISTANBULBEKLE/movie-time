1c30d2fc3841a11667bc651f17a8609787e0116b
ISTANBULBEKLE <ekipkalir@gmail.com>
2021-08-11 22:24:05 +0100

    navigation elements are added

--- App.js
+++ App.js
@@ -1,21 +1,20 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
 import { StyleSheet } from "react-native";
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
-import HomeScreen from "./src/screens/HomeScreen";
-import DetailsScreen from "./src/screens/DetailsScreen";
-import ImageScreen from "./src/screens/ImageScreen";
+import HomeScreen from "./screens/HomeScreen";
+import DetailsScreen from "./screens/DetailsScreen";
+import ImageScreen from "./screens/ImageScreen";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen component={HomeScreen} />
-        <Stack.Screen component={DetailsScreen} />
-        <Stack.Screen component={ImageScreen} />
+      <Stack.Navigator initialRouteName="Home">
+        <Stack.Screen name="Home" component={HomeScreen} />
+        <Stack.Screen name="Details" component={DetailsScreen} />
+        <Stack.Screen name="BigImageView" component={ImageScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
